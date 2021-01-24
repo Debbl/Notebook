957c971194b705605a099d3dba8db2256eebb374
Damain <61053131+Debbl@users.noreply.github.com>
2021-01-24 20:31:34 +0800

    Add Spring 04_Spring

---
+++ Spring/04_Spring/src/test/java/run/aiwan/MyTest02.java
@@ -0,0 +1,19 @@
+package run.aiwan;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import run.aiwan.ba02.Student;
+
+public class MyTest02 {
+
+    @Test
+    public void test01() {
+
+        String config = "ba02/applicationContext.xml";
+        ApplicationContext ac = new ClassPathXmlApplicationContext(config);
+
+        Student student = (Student) ac.getBean("student");
+        System.out.println(student);
+    }
+}
