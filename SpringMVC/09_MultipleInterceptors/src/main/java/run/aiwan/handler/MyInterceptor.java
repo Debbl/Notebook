2d3b2bba010635343d23f8a39e2554286f7fed5a
Damain <61053131+Debbl@users.noreply.github.com>
2021-02-12 22:34:27 +0800

    Add SpringMVC 多个拦截器的注册与执行

---
+++ SpringMVC/09_MultipleInterceptors/src/main/java/run/aiwan/handler/MyInterceptor.java
@@ -0,0 +1,25 @@
+package run.aiwan.handler;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("111111-拦截器的preHandle");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("111111-拦截器的postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("111111-拦截器的afterCompletion");
+    }
+}
